aa1b04dcde7c34b091167f2d0ae22c3b7377112a
oneMxz <Bogomolovmaksim608@gmail.com>
2025-10-04 00:12:59 +0700

    fix1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#ifndef VERSION_PROJECT
+#define VERSION_PROJECT "0"
+#endif
+
+int main(int, char**){
+    int a;
+    std::cout << "Hello, World! Version " << VERSION_PROJECT;
+    std::cout << "testttttt";
+    std::cin >> a;
+}
